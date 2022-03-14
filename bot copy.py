e6279c80059647f7ecd03f231622e8d01f66d1bd
Martin Jauffret <martinjfrt@hotmail.fr>
2022-03-14 21:30:31 +0100

    chrome drive issue

--- bot copy.py
+++ bot copy.py
@@ -30,7 +30,7 @@ else:
 with open('/Users/jauffret/code/MartinJ9678/paristennis/config.yaml') as f:
    data = yaml.load(f, Loader=yaml.FullLoader)
 
-def paris_tennis(couvert=True, hours=['21h','19h'], numero_court = None,name = "Elisabeth", day=day, profil='1', time_waiting = 8):
+def paris_tennis(couvert=True, hours=['19h'], numero_court = None,name = "Elisabeth", day=day, profil='1', time_waiting = 8):
     """Réservation terrain de tennis
     *****************************************************************
     couvert = False : Si le terrain doit être couvert ou non
@@ -47,7 +47,7 @@ def paris_tennis(couvert=True, hours=['21h','19h'], numero_court = None,name = "
         count+=1
         options = Options()
         options.add_argument("--window-size=1920,1080")
-        options.add_argument("--headless")
+        #options.add_argument("--headless")
         options.add_argument("--lang=fr")
         driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
         driver.get("https://tennis.paris.fr")
@@ -145,95 +145,125 @@ def paris_tennis(couvert=True, hours=['21h','19h'], numero_court = None,name = "
         #     wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//div[@class='dispo']")))
             
         disponibilites = driver.find_elements_by_class_name('date-item')
-
-        disponibilites[-1].find_element_by_class_name('date').click()
-        wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//div[@class='panel panel-default']")))
-
-        horaires = driver.find_elements_by_xpath("//div[@class='panel panel-default']")
+        #print(f"disponibilities after : {disponibilites}")
+        #print(f"text dispo after : {driver.find_elements_by_class_name('date-item')[-1].text}")
+        dispo_day = False
         
-        for hour in hours:
-            #print(hour)
-            for horaire in reversed(horaires):
-                tarif_trouve = False
-                if hour in horaire.find_element_by_class_name('panel-title').text:
-                    #print('hour trouve')
-                    horaire.find_element_by_class_name('panel-title').click()
-                    wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//div[@class='panel-collapse collapse in']")))
-                    courts = horaire.find_elements_by_class_name('tennis-court')
-                    #print(f'courts selenium : {courts}')
-                    for court in courts:
-                        court_trouve = False
-                        tarif_trouve = False
-                        #print(f"court infos : {court.find_element_by_class_name('price-description').text}")
-                        if tarif in court.find_element_by_class_name('price-description').text:
-                            tarif_trouve = True
-                            #print('tarif trouve')
-                            if numero_court!=None:
-                                if numero_court in court.text:
-                                    court.find_element_by_class_name('btn').click()
-                                    court_trouve = True
-                                if not court_trouve:
-                                    numero_court = courts[0].text[6:9]
-                                    courts[0].find_element_by_class_name('btn').click()
-                            else:
-                                numero_court = courts[0].text[6:9]
-                                courts[0].find_element_by_class_name('btn').click()
-                            break
-                if tarif_trouve:
-                    break
-            if tarif_trouve:
+        #import ipdb; ipdb.set_trace()
+        
+        for dispo in reversed(disponibilites) : 
+            #print(dispo.find_element_by_class_name('date').text)
+            if dispo.find_elements_by_class_name('dispo')!=[] and day in dispo.find_element_by_class_name('date').text:
+                dispo_day = True
+                #date = dispo.find_element_by_class_name('date').text
+                #meteo = dispo.find_element_by_tag_name('title').text
+                dispo.find_element_by_class_name('date').click()
+                wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//div[@class='panel panel-default']")))
+                #print("date: " + date + "\nmeteo: " + meteo)
                 break
-        
-        last_scrap = time.time() - start_hour
-        
-        print(f"last scrap : {last_scrap}")
-        
-        print(f"tarif : {tarif_trouve}")
-        
-        #print("date: " + date + "\nmeteo: " + meteo)
 
-        if not tarif_trouve:
-            print("aucun court dispo sur le tarif ou l'horaire demandé")
-            #import ipdb; ipdb.set_trace()
+        if dispo_day == False:
+            for dispo in reversed(disponibilites) :
+                if dispo.find_elements_by_class_name('dispo')!=[]:
+                    date = dispo.find_element_by_class_name('date').text
+                    meteo = dispo.find_element_by_tag_name('title').text
+                    dispo.find_element_by_class_name('date').click()
+                    wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//div[@class='panel panel-default']")))
+                    dispo_day = True
+                    print(date, meteo)
+                    break
+        
+        if dispo_day == False:
             driver.quit()
             continue
-                    
-        print(f"numero_court: {numero_court}/nhour: {hour}")
-        
-        NAME1 = 'JAUFFRET'
-        PRENOM1 = 'MARTIN'
-        EMAIL1 = 'martinjfrt@hotmail.fr'
 
-        wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//input[@name='player1']")))
-        infos_player1 = driver.find_elements_by_name('player1')
-        #print(infos_player1)
+        if dispo_day:
 
-        infos_player1[0].send_keys(NAME1)
-        infos_player1[1].send_keys(PRENOM1)
-        infos_player1[2].send_keys(EMAIL1)
+            horaires = driver.find_elements_by_xpath("//div[@class='panel panel-default']")
+            
+            for hour in hours:
+                #print(hour)
+                for horaire in reversed(horaires):
+                    tarif_trouve = False
+                    if hour in horaire.find_element_by_class_name('panel-title').text:
+                        #print('hour trouve')
+                        horaire.find_element_by_class_name('panel-title').click()
+                        time.sleep(1)
+                        courts = horaire.find_elements_by_class_name('tennis-court')
+                        #print(f'courts selenium : {courts}')
+                        for court in courts:
+                            court_trouve = False
+                            tarif_trouve = False
+                            #print(f"court infos : {court.find_element_by_class_name('price-description').text}")
+                            if tarif in court.find_element_by_class_name('price-description').text:
+                                tarif_trouve = True
+                                #print('tarif trouve')
+                                if numero_court!=None:
+                                    if numero_court in court.text:
+                                        court.find_element_by_class_name('btn').click()
+                                        court_trouve = True
+                                    if not court_trouve:
+                                        numero_court = courts[0].text[6:9]
+                                        courts[0].find_element_by_class_name('btn').click()
+                                else:
+                                    numero_court = courts[0].text[6:9]
+                                    courts[0].find_element_by_class_name('btn').click()
+                                break
+                    if tarif_trouve:
+                        break
+                if tarif_trouve:
+                    break
+            
+            last_scrap = time.time() - start_hour
+            
+            print(f"last scrap : {last_scrap}")
+            
+            print(f"tarif : {tarif_trouve}")
+            
+            print("date: " + date + "\nmeteo: " + meteo)
+
+            if not tarif_trouve:
+                print("aucun court dispo sur le tarif ou l'horaire demandé")
+                #import ipdb; ipdb.set_trace()
+                driver.quit()
+                continue
+                     
+            print(f"numero_court: {numero_court}/nhour: {hour}")
+            
+            NAME1 = 'JAUFFRET'
+            PRENOM1 = 'MARTIN'
+            EMAIL1 = 'martinjfrt@hotmail.fr'
+
+            wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//input[@name='player1']")))
+            infos_player1 = driver.find_elements_by_name('player1')
+            #print(infos_player1)
+
+            infos_player1[0].send_keys(NAME1)
+            infos_player1[1].send_keys(PRENOM1)
+            infos_player1[2].send_keys(EMAIL1)
 
 
-        driver.find_element_by_class_name('addPlayer').click()
-        
-        wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//input[@name='player2']")))
-        infos_player2 = driver.find_elements_by_name('player2')
+            driver.find_element_by_class_name('addPlayer').click()
+            
+            wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//input[@name='player2']")))
+            infos_player2 = driver.find_elements_by_name('player2')
 
 
-        NAME2 = 'DE CHAMBURE'
+            NAME2 = 'DE CHAMBURE'
 
-        PRENOM2 = 'CYPRIEN'
+            PRENOM2 = 'CYPRIEN'
 
-        infos_player2[0].send_keys(NAME2)
-        infos_player2[1].send_keys(PRENOM2)
-        
-        driver.find_element_by_class_name('addPlayer').submit()
-        
-        wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//div[@class='price']")))
-        driver.find_element_by_class_name("price").click()
+            infos_player2[0].send_keys(NAME2)
+            infos_player2[1].send_keys(PRENOM2)
+            
+            driver.find_element_by_class_name('addPlayer').submit()
+            
+            wait.until(ec.visibility_of_all_elements_located((By.XPATH, "//div[@class='price']")))
+            driver.find_element_by_class_name("price").click()
 
-        driver.find_element_by_id('submit').click()
-        
-        resa_prise = True
+            driver.find_element_by_id('submit').click()
+            
+            resa_prise = True
             
 if __name__=='__main__':
-    paris_tennis(profil="1",time_waiting=8)
+    paris_tennis(profil="1",time_waiting=12, hours=['15h'])
